c5d7c933b2b82ab3cbb790bc2050b05d53953eb0
wgff0012-dev <wgff.0012@gmail.com>
2026-08-27 01:40:46 -0300

    mandei o funcionario e o clt

--- Exercicio2/src/main/java/exercicio2/Funcionario.java
+++ Exercicio2/src/main/java/exercicio2/Funcionario.java
@@ -1,5 +1,37 @@
 package exercicio2;
 
-public class Funcionario {
+import java.time.LocalDate;
 
+public class Funcionario {
+    private String nome;
+    private String matricula;
+    private double salarioBase;
+    private LocalDate dataAdmissao;
+
+    public Funcionario(String nome, String matricula, double salarioBase, LocalDate dataAdmissao) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salarioBase = salarioBase;
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    // Retorna o salário-base
+    public double calcularSalario() {
+        return this.salarioBase;
+    }
+
+    // Calcula 8% do salário-base
+    public double calcularDesconto() {
+        return this.salarioBase * 0.08;
+    }
+    
+    @Override
+    public String toString() {
+        return "Funcionario" +
+                "\nNome= " + this.nome + 
+                "\nMatrícula= " + this.matricula + 
+                "\nSalário Base=R$ " + String.format("%.2f", calcularSalario()) +
+                "\nData de Admissão= " + this.dataAdmissao +
+                "\nSalário Líquido=R$ " + String.format("%.2f", calcularSalario() - calcularDesconto());
+    }
 }
--- Exercicio2/src/main/java/exercicio2/FuncionarioCLT.java
+++ Exercicio2/src/main/java/exercicio2/FuncionarioCLT.java
@@ -1,5 +1,34 @@
 package exercicio2;
 
-public class FuncionarioCLT {
+import java.time.LocalDate;
 
+public class FuncionarioCLT extends Funcionario {
+    private double valeTransporte;
+    private double valeAlimentacao;
+
+    public FuncionarioCLT(String nome, String matricula, double salarioBase, LocalDate dataAdmissao, double valeTransporte, double valeAlimentacao) {
+        super(nome, matricula, salarioBase, dataAdmissao);
+        this.valeTransporte = valeTransporte;
+        this.valeAlimentacao = valeAlimentacao;
+    }
+
+    //inclui salário-base (via super) + benefícios
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + this.valeTransporte + this.valeAlimentacao;
+    }
+
+    //mantém o desconto do pai (via super) + R$ 50,00
+    @Override
+    public double calcularDesconto() {
+        return super.calcularDesconto() + 50.0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "\nCLT:" +
+               "\nVale Transporte = R$ " + this.valeTransporte +
+               "\nVale Alimentação = R$ " + this.valeAlimentacao;
+    }
 }
